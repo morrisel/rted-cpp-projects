f873c6ef5fa55e570a5ddcc01104d8d2269b1c1b
morris <morris.elkanaev@gmail.com>
2024-04-10 19:58:27 +0300

    delete task2_vet_program folder

--- task2_vet_program/Animal.cpp
+++
@@ -1,8 +0,0 @@
-// Animal.cpp
-#include "Animal.hpp"
-
-Animal::Animal(std::string name, double temp, double breath, double heart)
-	: name(std::move(name)), temp(temp), breath(breath), heart(heart)
-{
-}
-
